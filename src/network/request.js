b3f7082624d764344e9116f51327d762710614d5
lz-126 <bpmf2@outlook.com>
2020-04-28 18:02:36 +0800

    更改了request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,2 +1,11 @@
 import axios from 'axios'
-export request 
+
+export const request = (config) => {
+  const instance1 = axios.create({
+    // baseURL:'/data/home/multidata',
+    timeout: 5000
+  })
+  // instance1.interceptors.response.use()
+  return instance1(config)
+}
+
